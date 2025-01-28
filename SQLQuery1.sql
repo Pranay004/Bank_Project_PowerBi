b933daf53afb3ab4aeef80f0ed76a760af5dec44
Pranay004 <87721093+Pranay004@users.noreply.github.com>
2025-01-28 18:11:24 +0530

    Project Files
    Bank Transactions Analysis Project
    This project is a comprehensive analysis of bank transaction data, focusing on key insights such as customer behavior, transaction trends, and profitability metrics. Built using SQL, it simulates real-world banking scenarios, including transaction tracking, reporting, and performance analysis.

---
+++ SQLQuery1.sql
@@ -0,0 +1,34 @@
+--This is for our automation Project
+
+SELECT * FROM [Banks].[Transactions Table]
+
+INSERT INTO [Banks].[Transactions Table] (transaction_id, customer_id, transaction_date, transaction_type, transaction_amount, channels, currency)
+VALUES ('501', '6', '2023-12-14', 'Credit', '145556', 'Apps', 'Naira'),
+('502', '10', '2023-12-14', 'Credit', '245556', 'Apps', 'Naira'),
+('503', '6', '2023-12-14', 'Debit', '149556', 'Web', 'Naira'),
+('504', '11', '2023-12-14', 'Credit', '145556', 'Web', 'Naira'),
+('505', '11', '2023-12-14', 'Debit', '145556', 'Cards', 'Naira'),
+('506', '13', '2023-12-14', 'Debit', '246756', 'Apps', 'Naira'),
+('507', '20', '2023-12-15', 'Credit', '315556', 'Apps', 'Naira'),
+('508', '12', '2023-12-15', 'Credit', '915556', 'Apps', 'Naira'),
+('509', '17', '2023-12-15', 'Debit', '390556', 'Apps', 'Naira'),
+('510', '17', '2023-12-15', 'Debit', '536556', 'Apps', 'Naira'),
+('511', '12', '2023-12-15', 'Credit', '205556', 'Apps', 'Naira'),
+('512', '19', '2023-12-15', 'Debit', '245556', 'Apps', 'Naira'),
+('513', '9', '2023-12-16', 'Credit', '445556', 'Apps', 'Naira'),
+('514', '10', '2023-12-16', 'Debit', '335556', 'Apps', 'Naira'),
+('515', '12', '2023-12-16', 'Debit', '144456', 'Apps', 'Naira'),
+('516', '5', '2023-12-17', 'Credit', '125556', 'Apps', 'Naira'),
+('517', '1', '2023-12-17', 'Credit', '241156', 'Apps', 'Naira'),
+('518', '3', '2023-12-17', 'Debit', '123556', 'Apps', 'Naira')
+
+
+
+INSERT INTO [Banks].[Transactions Table] (transaction_id, customer_id, transaction_date, transaction_type, transaction_amount, channels, currency)
+VALUES('519', '11', '2023-12-18', 'Debit', '145556', 'Cards', 'Naira'),
+('520', '13', '2023-12-18', 'Debit', '6756', 'Apps', 'Naira'),
+('521', '20', '2023-12-18', 'Credit', '15556', 'Apps', 'Naira'),
+('522', '12', '2023-12-18', 'Credit', '915556', 'Apps', 'Naira'),
+('523', '17', '2023-12-18', 'Debit', '390556', 'Apps', 'Naira'),
+('524', '17', '2023-12-18', 'Debit', '6556', 'Apps', 'Naira'),
+('525', '12', '2023-12-18', 'Credit', '205556', 'Apps', 'Naira')
